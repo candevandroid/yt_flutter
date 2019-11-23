30a402df957878c343106aae78f7d06485d700ec
pdaniel37 <pedrodaniel@candev.com.br>
2019-11-23 02:37:51 -0300

    adding appBar

---
+++ lib/Home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            iconTheme: IconThemeData(
+                color: Colors.grey,
+                opacity: 1
+            ),
+            backgroundColor: Colors.white,
+            title: Image.asset(
+                "images/youtube.png",
+                width: 98,
+                height: 22,
+            ),
+            actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.videocam),
+                    onPressed: (){},
+                ),
+                IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: (){},
+                ),
+                IconButton(
+                    icon: Icon(Icons.videocam),
+                    onPressed: (){},
+                )
+            ],
+        ),
+    );
+  }
+}
